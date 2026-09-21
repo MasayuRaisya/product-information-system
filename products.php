59942559b982578dbf536b04d42c396a3b81fef6
MasayuRaisya <masayu.250180175@mhs.unimal.ac.id>
2026-09-21 17:41:30 +0700

    Add initial product data to products.php

--- products.php
+++ products.php
@@ -1,1 +1,14 @@
+<?php
 
+$products = [
+    [
+        "id" => 1,
+        "nama" => "Cabai Merah",
+        "kategori" => "Pertanian",
+        "harga" => 45000,
+        "stok" => 15,
+        "deskripsi" => "Cabai merah segar berkualitas untuk kebutuhan rumah tangga."
+    ]
+];
+
+?>
