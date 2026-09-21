8949c2799c415d4da8f073b8c67cb41556601925
MasayuRaisya <masayu.250180175@mhs.unimal.ac.id>
2026-09-21 21:53:52 +0700

    Update products.php

--- products.php
+++ products.php
@@ -1,14 +1,28 @@
 <?php
+// products.php - Menampung data komoditas dalam multidimensional array
 
 $products = [
     [
-        "id" => 1,
-        "nama" => "Cabai Merah",
-        "kategori" => "Pertanian",
-        "harga" => 45000,
-        "stok" => 15,
-        "deskripsi" => "Cabai merah segar berkualitas untuk kebutuhan rumah tangga."
-    ]
-];
-
-?>
+        "id" => 101,
+        "nama" => "Laptop ASUS ROG Strix",
+        "kategori" => "Laptop",
+        "harga" => 22500000,
+        "stok" => 5,
+        "deskripsi" => "Laptop gaming layar 16 inci, RTX 4060, RAM 16GB."
+    ],
+    [
+        "id" => 102,
+        "nama" => "Smartphone Samsung S24",
+        "kategori" => "Smartphone",
+        "harga" => 15000000,
+        "stok" => 2, // Stok kritis (< 3)
+        "deskripsi" => "Layar AMOLED 120Hz, kamera 50MP, AI features."
+    ],
+    [
+        "id" => 103,
+        "nama" => "Mouse Wireless Logitech",
+        "kategori" => "Aksesori",
+        "harga" => 350000,
+        "stok" => 12,
+        "deskripsi" => "Mouse ergononis tanpa kabel dengan baterai tahan lama."
+    ],
