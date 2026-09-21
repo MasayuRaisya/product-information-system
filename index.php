d0b63de25ee013f3e440b68b51ee4cef15b9c4b8
MasayuRaisya <masayu.250180175@mhs.unimal.ac.id>
2026-09-21 21:41:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'product.php';
-require_once 'functions.php';
+require_once "product.php";
+require_once "functions.php";
 
 ?>
 
@@ -9,68 +9,46 @@ require_once 'functions.php';
 <html>
 <head>
     <title>Product Information System</title>
-
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        th, td {
-            border: 1px solid black;
-            padding: 10px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-
-        .stok-kritis {
-            background-color: #ffcccc;
-        }
-    </style>
 </head>
 
 <body>
 
-    <h1>Product Information System</h1>
+<h1>Product Information System</h1>
 
-    <table>
+<table border="1" cellpadding="10" cellspacing="0">
+
+    <tr>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>Kategori</th>
+        <th>Harga</th>
+        <th>Stok</th>
+        <th>Deskripsi</th>
+        <th>Total Nilai Stok</th>
+    </tr>
+
+    <?php foreach ($products as $product) { ?>
+
+        <?php
+        $total = hitungTotalNilaiStok(
+            $product["harga"],
+            $product["stok"]
+        );
+        ?>
+
         <tr>
-            <th>ID</th>
-            <th>Nama</th>
-            <th>Kategori</th>
-            <th>Harga</th>
-            <th>Stok</th>
-            <th>Deskripsi</th>
-            <th>Total Nilai Stok</th>
+            <td><?php echo $product["id"]; ?></td>
+            <td><?php echo $product["nama"]; ?></td>
+            <td><?php echo $product["kategori"]; ?></td>
+            <td>Rp <?php echo number_format($product["harga"], 0, ",", "."); ?></td>
+            <td><?php echo $product["stok"]; ?></td>
+            <td><?php echo $product["deskripsi"]; ?></td>
+            <td>Rp <?php echo number_format($total, 0, ",", "."); ?></td>
         </tr>
 
-        <?php foreach ($products as $product): ?>
+    <?php } ?>
 
-            <?php
-                $totalNilaiStok = hitungTotalNilaiStok(
-                    $product['harga'],
-                    $product['stok']
-                );
-
-                $stokKritis = cekStokKritis($product['stok']);
-            ?>
-
-            <tr class="<?= $stokKritis ? 'stok-kritis' : '' ?>">
-                <td><?= $product['id'] ?></td>
-                <td><?= $product['nama'] ?></td>
-                <td><?= $product['kategori'] ?></td>
-                <td>Rp <?= number_format($product['harga'], 0, ',', '.') ?></td>
-                <td><?= $product['stok'] ?></td>
-                <td><?= $product['deskripsi'] ?></td>
-                <td>Rp <?= number_format($totalNilaiStok, 0, ',', '.') ?></td>
-            </tr>
-
-        <?php endforeach; ?>
-
-    </table>
+</table>
 
 </body>
 </html>
