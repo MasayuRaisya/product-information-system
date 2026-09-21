e7faa565ec0abe97684d45422e89cb221b8727af
MasayuRaisya <masayu.250180175@mhs.unimal.ac.id>
2026-09-21 17:44:05 +0700

    Add stock value calculation and critical stock check functions

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
+<?php
 
+// Fungsi untuk menghitung total nilai stok
+function hitungTotalNilaiStok($harga, $stok)
+{
+    return $harga * $stok;
+}
+
+// Fungsi untuk menentukan apakah stok kritis
+function cekStokKritis($stok)
+{
+    return $stok < 3;
+}
+
+?>
